98c1654b74cde3a650625b21cf94c15b84481185
ChoiGaYoung <choidh1261@naver.com>
2023-05-05 01:14:55 +0900

    create bottomseet

---
+++ app/src/main/java/com/cookandroid/capstone/BottomSheet_Calendar.java
@@ -0,0 +1,30 @@
+package com.cookandroid.capstone;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+public class BottomSheet_Calendar extends BottomSheetDialogFragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bottom_sheet__calendar, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btnFinish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+    }
+}
