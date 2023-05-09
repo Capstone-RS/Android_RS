77830c5a1e8dc29b9830d0558c0e603cb81baf8c
solbeen <solyoyo@naver.com>
2023-05-10 00:04:28 +0900

    feat : workdetailedit의 spinner 연결

--- app/src/main/java/com/cookandroid/capstone/WorkDetailEditActivity.java
+++ app/src/main/java/com/cookandroid/capstone/WorkDetailEditActivity.java
@@ -1,6 +1,8 @@
 package com.cookandroid.capstone;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,18 @@ public class WorkDetailEditActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_workdetail_edit); // workdata xml이랑 연결된 자바파일이라는 뜻
 
+        Spinner spnPay = (Spinner) findViewById(R.id.spnPay);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.array_workdatadetailedit_howpay, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnPay.setAdapter(adapter);
 
+        Spinner spnRestTime = (Spinner) findViewById(R.id.spnRestTime);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.array_workdatadetailedit_rest, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnRestTime.setAdapter(adapter1);
+
+        
     }
 }
