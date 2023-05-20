77b4590d52660f80d5d7d95da270f0d37fde67d6
ahnjisu <an1302@hs.ac.kr>
2023-05-20 15:45:55 +0900

    feat : 홈 수정, 프로필 알림설정 바텀시트 연결, 바텀시트 모서리 둥글게

--- app/src/main/java/com/cookandroid/capstone/BottomSheet_Push_Notification.java
+++ app/src/main/java/com/cookandroid/capstone/BottomSheet_Push_Notification.java
@@ -1,5 +1,6 @@
 package com.cookandroid.capstone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,23 +8,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class BottomSheet_Push_Notification extends BottomSheetDialogFragment {
+    public class BottomSheet_Push_Notification extends BottomSheetDialogFragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bottom_sheet__push_notification, container, false);
-    }
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_bottom_sheet__push_notification, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() { // 수정해야함..
-            @Override
-            public void onClick(View view) {
-                dismiss();
-            }
-        });
+            return view;
+        }
+
     }
-}
--- app/src/main/java/com/cookandroid/capstone/Fragment/ProfileFragment.java
+++ app/src/main/java/com/cookandroid/capstone/Fragment/ProfileFragment.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
+import com.cookandroid.capstone.BottomSheet_Push_Notification;
 import com.cookandroid.capstone.R;
 
 public class ProfileFragment extends Fragment {
 
+    private LinearLayout notificationSettingsLayout;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_profile, container, false);
+
+        notificationSettingsLayout = view.findViewById(R.id.notification_settings_layout);
+        notificationSettingsLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetDialogFragment();
+            }
+        });
+
+        return view;
+    }
+    private void showBottomSheetDialogFragment() {
+        BottomSheet_Push_Notification bottomSheet = new BottomSheet_Push_Notification();
+        bottomSheet.show(getParentFragmentManager(), "push_notification_bottom_sheet");
     }
 }
