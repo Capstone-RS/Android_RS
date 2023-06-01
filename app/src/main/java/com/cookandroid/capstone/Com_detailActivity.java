449f4a6226cacbdec000f69f666c8131ec88b71b
ppjjhh2001 <ppjjhh2001@hs.ac.kr>
2023-06-01 12:00:15 +0900

    feat : 오류수정

--- app/src/main/java/com/cookandroid/capstone/Com_detailActivity.java
+++ app/src/main/java/com/cookandroid/capstone/Com_detailActivity.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Com_detailActivity extends Activity {
 
-    Button btn_back;
+    TextView btn_back;
     Button btn_reg;
 
     @Override
@@ -18,7 +19,7 @@ public class Com_detailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_detail);
 
-        btn_back = findViewById(R.id.btnBack);
+        btn_back = findViewById(R.id.btn_Back);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/cookandroid/capstone/Fragment/CommunityFragment.java
+++ app/src/main/java/com/cookandroid/capstone/Fragment/CommunityFragment.java
@@ -1,20 +1,32 @@
 package com.cookandroid.capstone.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.cookandroid.capstone.Com_listActivity;
 import com.cookandroid.capstone.R;
+import com.cookandroid.capstone.WorkDataActivity;
 
 public class CommunityFragment extends Fragment {
 
+    TextView com1_1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        com1_1 = getView().findViewById(R.id.com1_1);
+        com1_1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), Com_listActivity.class);
+                startActivity(intent);
+            }
+        });
         return inflater.inflate(R.layout.activity_community_main, container, false);
     }
 }
