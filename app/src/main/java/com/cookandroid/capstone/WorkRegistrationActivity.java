2c6e3cdd7e15bcec0d6a7b7291a94c98ec543c21
ChoiGYoung <choidh1261@naver.com>
2023-06-26 19:54:55 +0900

    feat : 근무 등록 xml , java 생성

---
+++ app/src/main/java/com/cookandroid/capstone/WorkRegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.capstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WorkRegistrationActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
