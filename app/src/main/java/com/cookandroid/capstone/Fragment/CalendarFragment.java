330f3127d9c13179a73ae2823848ab3c72c1da7d
ChoiGYoung <choidh1261@naver.com>
2023-06-28 20:33:55 +0900

    feat : CalendarFragment 리스트뷰 수정
    리스트뷰에 데이터가 여러번 반복하여 출력되는 현상 수정

--- app/src/main/java/com/cookandroid/capstone/Fragment/CalendarFragment.java
+++ app/src/main/java/com/cookandroid/capstone/Fragment/CalendarFragment.java
@@ -45,13 +45,13 @@ public class CalendarFragment extends Fragment {
     private MaterialCalendarView calendarView;
     private TextView selectedDateTextView;
     ListView workList;
-    CustomAdapter adapter ;
+    CustomAdapter adapter;
     Button btnAdd;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        adapter = new CustomAdapter(getContext(), R.layout.calendar_customlistview);
+        adapter = new CustomAdapter(getContext(), R.layout.calendar_customlistview, new ArrayList<String>());
     }
 
     @Override
@@ -61,7 +61,6 @@ public class CalendarFragment extends Fragment {
         selectedDateTextView = v.findViewById(R.id.btn);
         workList = v.findViewById(R.id.workList);
         btnAdd = v.findViewById(R.id.btnAdd);
-        adapter = new CustomAdapter(getContext(), R.layout.calendar_customlistview);
 
         //근무 추가 버튼
         btnAdd.setOnClickListener(new View.OnClickListener() {
@@ -72,14 +71,12 @@ public class CalendarFragment extends Fragment {
             }
         });
 
-
         if (workList == null) {
             Log.e("CalendarFragment", "workList is null");
         } else {
             workList.setAdapter(adapter);
             // 이후의 코드 계속 실행
         }
-
         calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
@@ -98,13 +95,16 @@ public class CalendarFragment extends Fragment {
                             DataSnapshot datesSnapshot = snapshot.child("dates");
                             if (datesSnapshot.exists()) {
                                 for (DataSnapshot dateSnapshot : datesSnapshot.getChildren()) {
-                                    String date = dateSnapshot.getValue(String.class);
-                                    if (date != null && date.trim().equals(formattedDate)) {
-                                        String name = snapshot.child("name").getValue(String.class);
-                                        if (name != null) {
-                                            startTimes.add(name);
+                                    HashMap<String, Object> dateData = dateSnapshot.getValue(new GenericTypeIndicator<HashMap<String, Object>>() {});
+                                    if (dateData != null) {
+                                        String date = (String) dateData.get("date");
+                                        if (date != null && date.trim().equals(formattedDate)) {
+                                            String name = snapshot.child("name").getValue(String.class);
+                                            if (name != null) {
+                                                startTimes.add(name);
+                                            }
+                                            break;
                                         }
-                                        break;
                                     }
                                 }
                             } else {
@@ -129,16 +129,19 @@ public class CalendarFragment extends Fragment {
             }
         });
 
-
         return v;
     }
 
     public class CustomAdapter extends ArrayAdapter<String> {
         private Context context;
+        private List<String> items;
+        private DatabaseReference databaseRef;
 
-        public CustomAdapter(Context context, int resource) {
-            super(context, resource);
+        public CustomAdapter(Context context, int resource, List<String> items) {
+            super(context, resource, items);
             this.context = context;
+            this.items = items;
+            this.databaseRef = FirebaseDatabase.getInstance().getReference("Data");
         }
 
         @Override
@@ -147,75 +150,79 @@ public class CalendarFragment extends Fragment {
             View rowView = inflater.inflate(R.layout.calendar_customlistview, parent, false);
 
             TextView textView1 = rowView.findViewById(R.id.textView1);
-            TextView textView2 = rowView.findViewById(R.id.textView2); // 추가된 부분
+            TextView textView2 = rowView.findViewById(R.id.textView2);
 
-            String itemName = getItem(position); // 해당 position에 있는 데이터 가져오기
-            textView1.setText(itemName); // 데이터를 textView1에 설정
-            textView2.setText("");
+            String itemName = items.get(position);
+            textView1.setText(itemName);
 
-            DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("Data");
             Query query = databaseRef.orderByChild("name").equalTo(itemName);
 
             query.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    DecimalFormat decimalFormat = new DecimalFormat("#,##0");// 소수점 이하 자리수 제거
-                    textView2.setText("");
+                    DecimalFormat decimalFormat = new DecimalFormat("#,##0");
+                    double totalEarnings = 0.0;
 
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        String money = snapshot.child("money").getValue(String.class);
-                        String startTime = snapshot.child("startTime").getValue(String.class);
-                        String endTime = snapshot.child("endTime").getValue(String.class);
-                        String restTime = snapshot.child("RestTime").getValue(String.class);
+                    // 첫 번째 일치하는 자식 데이터 가져오기
+                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                    DataSnapshot datesSnapshot = firstChild.child("dates");
+                    if (datesSnapshot.exists()) {
 
-                        // startTime과 endTime을 시간 형식("HH:mm")에서 분으로 변환
-                        String[] startTimeParts = startTime.split(":");
-                        int startHour = Integer.parseInt(startTimeParts[0]);
-                        int startMinute = Integer.parseInt(startTimeParts[1]);
+                        for (DataSnapshot dateSnapshot : datesSnapshot.getChildren()) {
+                            String money = firstChild.child("money").getValue(String.class);
+                            String startTime = dateSnapshot.child("startTime").getValue(String.class);
+                            String endTime = dateSnapshot.child("endTime").getValue(String.class);
+                            String restTime = firstChild.child("RestTime").getValue(String.class);
 
-                        String[] endTimeParts = endTime.split(":");
-                        int endHour = Integer.parseInt(endTimeParts[0]);
-                        int endMinute = Integer.parseInt(endTimeParts[1]);
+                            // startTime과 endTime을 시간 형식("HH:mm")에서 분으로 변환
+                            String[] startTimeParts = startTime.split(":");
+                            int startHour = Integer.parseInt(startTimeParts[0]);
+                            int startMinute = Integer.parseInt(startTimeParts[1]);
 
-                        // restTime에서 "분" 문자열 제거 및 숫자 값만 추출
-                        int restMinutes = 0; // 기본값 설정
+                            String[] endTimeParts = endTime.split(":");
+                            int endHour = Integer.parseInt(endTimeParts[0]);
+                            int endMinute = Integer.parseInt(endTimeParts[1]);
 
-                        if (restTime != null) {
-                            String restTimeString = restTime.replace("분", "").trim();
-                            restMinutes = Integer.parseInt(restTimeString);
+                            // restTime에서 "분" 문자열 제거 및 숫자 값만 추출
+                            int restMinutes = 0; // 기본값 설정
+
+                            if (restTime != null) {
+                                String restTimeString = restTime.replace("분", "").trim();
+                                restMinutes = Integer.parseInt(restTimeString);
+                            }
+
+                            // startTime과 endTime을 분으로 변환
+                            int startTotalMinutes = startHour * 60 + startMinute;
+                            int endTotalMinutes = endHour * 60 + endMinute;
+
+                            // 휴게 시간을 분으로 변환
+                            int restTotalMinutes = restMinutes;
+
+                            // 근무 시간 계산 (분 단위)에서 휴게 시간 제외
+                            int workMinutes = endTotalMinutes - startTotalMinutes - restTotalMinutes;
+
+                            // 시급 계산
+                            double hourlyRate = Double.parseDouble(money);
+
+                            // 근무 시간을 시간 단위로 변환
+                            double workHours = workMinutes / 60.0;
+
+                            // 총 급여 계산
+                            double earnings = hourlyRate * workHours;
+
+                            totalEarnings = earnings;
                         }
 
-                        // startTime과 endTime을 분으로 변환
-                        int startTotalMinutes = startHour * 60 + startMinute;
-                        int endTotalMinutes = endHour * 60 + endMinute;
-
-                        // 휴게 시간을 분으로 변환
-                        int restTotalMinutes = restMinutes;
-
-                        // 근무 시간 계산 (분 단위)에서 휴게 시간 제외
-                        int workMinutes = endTotalMinutes - startTotalMinutes - restTotalMinutes;
-
-                        // 시급 계산
-                        double hourlyRate = Double.parseDouble(money);
-
-                        // 근무 시간을 시간 단위로 변환
-                        double workHours = workMinutes / 60.0;
-
-                        // 총 급여 계산
-                        double earnings = hourlyRate * workHours;
-
-                        // 시급을 원하는 형식으로 포맷팅
-                        String formattedEarnings = decimalFormat.format(earnings);
-
-                        // 이후의 코드는 동일하게 유지
-                        String previousText = textView2.getText().toString();
-                        String newText = previousText + formattedEarnings + "\n";
+                        String formattedTotalEarnings = decimalFormat.format(totalEarnings);
+                        String newText = formattedTotalEarnings;
                         textView2.setText(newText);
                     }
                 }
+
+
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
-                    // 에러 처리 로직을 작성해주세요.
+                    // 에러 처리
                 }
             });
 
