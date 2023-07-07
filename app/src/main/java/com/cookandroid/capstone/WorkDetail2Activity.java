4a943474adc325fee2e66d4b1d9e2d48118592b5
ChoiGYoung <choidh1261@naver.com>
2023-07-07 16:02:26 +0900

    feat : 근무 수정 페이지에 데이터 출력 및 해당 근무 삭제 구현
    근무상세 페이지에서 수정 버튼을 눌렀을 때 근무 수정 페이지로 이동하면서 저장된 데이터를 출력.
    삭제 버튼을 누르면 firebase에 저장되어 있던 해당 데이터가 삭제

--- app/src/main/java/com/cookandroid/capstone/WorkDetail2Activity.java
+++ app/src/main/java/com/cookandroid/capstone/WorkDetail2Activity.java
@@ -40,6 +40,8 @@ public class WorkDetail2Activity extends AppCompatActivity {
         TextView endTime = findViewById(R.id.endTime);
         TextView restTime = findViewById(R.id.restTime);
 
+        TextView btnCorrect = findViewById(R.id.btnCorrect);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,97 +52,105 @@ public class WorkDetail2Activity extends AppCompatActivity {
         });
 
         Intent intent = getIntent();
-        if (intent != null) {
-            String itemName = intent.getStringExtra("itemName");
-            String formattedEarnings = intent.getStringExtra("formattedEarnings");
-            String selectedDate = intent.getStringExtra("selectedDate");
-            // 값 표시하기
-            if (itemName != null) {
-                name.setText(itemName);
-            }
-            if (formattedEarnings != null) {
-                money.setText(formattedEarnings + "원");
-            }
-            if (selectedDate != null) {
-                date.setText(selectedDate);
+        final String itemName = intent.getStringExtra("itemName");
+        final String formattedEarnings = intent.getStringExtra("formattedEarnings");
+        final String selectedDate = intent.getStringExtra("selectedDate");
+        // 값 표시하기
+        if (itemName != null) {
+            name.setText(itemName);
+        }
+        if (formattedEarnings != null) {
+            money.setText(formattedEarnings + "원");
+        }
+        if (selectedDate != null) {
+            date.setText(selectedDate);
 
-                // Firebase에서 데이터 가져오기
-                DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("Data");
-                Query query = databaseRef.orderByChild("name").equalTo(itemName);
-                query.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            DataSnapshot datesSnapshot = snapshot.child("dates");
-                            if (datesSnapshot.exists()) {
-                                ArrayList<HashMap<String, String>> groupData = new ArrayList<>();
-                                ArrayList<ArrayList<HashMap<String, String>>> childData = new ArrayList<>();
+            // Firebase에서 데이터 가져오기
+            DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("Data");
+            Query query = databaseRef.orderByChild("name").equalTo(itemName);
+            query.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        DataSnapshot datesSnapshot = snapshot.child("dates");
+                        if (datesSnapshot.exists()) {
+                            ArrayList<HashMap<String, String>> groupData = new ArrayList<>();
+                            ArrayList<ArrayList<HashMap<String, String>>> childData = new ArrayList<>();
 
-                                // [부모 리스트]
-                                HashMap<String, String> groupA = new HashMap<>();
-                                groupA.put("group", "총 급여");
-                                groupData.add(groupA);
+                            // [부모 리스트]
+                            HashMap<String, String> groupA = new HashMap<>();
+                            groupA.put("group", "총 급여");
+                            groupData.add(groupA);
 
-                                // [자식 리스트]
-                                ArrayList<HashMap<String, String>> childListA = new ArrayList<>();
+                            // [자식 리스트]
+                            ArrayList<HashMap<String, String>> childListA = new ArrayList<>();
 
-                                for (DataSnapshot dateSnapshot : datesSnapshot.getChildren()) {
-                                    String dateValue = dateSnapshot.child("date").getValue(String.class);
-                                    String startTimeValue = dateSnapshot.child("startTime").getValue(String.class);
-                                    String endTimeValue = dateSnapshot.child("endTime").getValue(String.class);
-                                    String restTimeValue = dateSnapshot.child("restTime").getValue(String.class);
-                                    String moneyValue = dateSnapshot.child("money").getValue(String.class);
+                            for (DataSnapshot dateSnapshot : datesSnapshot.getChildren()) {
+                                String dateValue = dateSnapshot.child("date").getValue(String.class);
+                                String startTimeValue = dateSnapshot.child("startTime").getValue(String.class);
+                                String endTimeValue = dateSnapshot.child("endTime").getValue(String.class);
+                                String restTimeValue = dateSnapshot.child("restTime").getValue(String.class);
+                                String moneyValue = dateSnapshot.child("money").getValue(String.class);
 
-                                    if (dateValue != null && dateValue.trim().equals(selectedDate.trim())) {
-                                        // 자식 데이터를 HashMap에 추가
-                                        HashMap<String, String> childDataMap = new HashMap<>();
-                                        childDataMap.put("data", "시급: " + moneyValue+"원");
-                                        childListA.add(childDataMap);
+                                if (dateValue != null && dateValue.trim().equals(selectedDate.trim())) {
+                                    // 자식 데이터를 HashMap에 추가
+                                    HashMap<String, String> childDataMap = new HashMap<>();
+                                    childDataMap.put("data", "시급: " + moneyValue + "원");
+                                    childListA.add(childDataMap);
 
-                                        HashMap<String, String> childDataMap2 = new HashMap<>();
-                                        childDataMap2.put("data", "근무 수당: " + formattedEarnings +"원");
-                                        childListA.add(childDataMap2);
+                                    HashMap<String, String> childDataMap2 = new HashMap<>();
+                                    childDataMap2.put("data", "근무 수당: " + formattedEarnings + "원");
+                                    childListA.add(childDataMap2);
 
-                                        // 값 표시하기
-                                        if (startTimeValue != null) {
-                                            startTime.setText(startTimeValue);
-                                        }
-                                        if (endTimeValue != null) {
-                                            endTime.setText(endTimeValue);
-                                        }
-                                        if (restTimeValue != null) {
-                                            restTime.setText(restTimeValue);
-                                        }
+                                    // 값 표시하기
+                                    if (startTimeValue != null) {
+                                        startTime.setText(startTimeValue);
+                                    }
+                                    if (endTimeValue != null) {
+                                        endTime.setText(endTimeValue);
+                                    }
+                                    if (restTimeValue != null) {
+                                        restTime.setText(restTimeValue);
                                     }
                                 }
-
-                                childData.add(childListA);
-
-                                // [부모 리스트와 자식 리스트를 포함한 Adapter를 생성]
-                                SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
-                                        WorkDetail2Activity.this, groupData,
-                                        android.R.layout.simple_expandable_list_item_1,
-                                        new String[]{"group"}, new int[]{android.R.id.text1},
-                                        childData, android.R.layout.simple_expandable_list_item_2,
-                                        new String[]{"data"}, new int[]{android.R.id.text1});
-
-                                // [ExpandableListView 에 Adapter 설정]
-                                ExpandableListView listView = findViewById(R.id.expandableListView);
-                                listView.setAdapter(adapter);
-
-                                break;
-                            } else {
-                                Log.e("WorkDetail2Activity", "dates is null");
                             }
+
+                            childData.add(childListA);
+
+                            // [부모 리스트와 자식 리스트를 포함한 Adapter를 생성]
+                            SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                                    WorkDetail2Activity.this, groupData,
+                                    android.R.layout.simple_expandable_list_item_1,
+                                    new String[]{"group"}, new int[]{android.R.id.text1},
+                                    childData, android.R.layout.simple_expandable_list_item_2,
+                                    new String[]{"data"}, new int[]{android.R.id.text1});
+
+                            // [ExpandableListView 에 Adapter 설정]
+                            ExpandableListView listView = findViewById(R.id.expandableListView);
+                            listView.setAdapter(adapter);
+
+                            break;
+                        } else {
+                            Log.e("WorkDetail2Activity", "dates is null");
                         }
                     }
+                }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        // 에러 처리 로직을 작성해주세요.
-                    }
-                });
-            }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // 에러 처리 로직을 작성해주세요.
+                }
+            });
         }
+
+        btnCorrect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WorkDetailEditActivity.class);
+                intent.putExtra("itemName", itemName);
+                intent.putExtra("selectedDate", selectedDate);
+                startActivity(intent);
+            }
+        });
     }
 }
