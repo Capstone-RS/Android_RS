fa34c9a5dda2f3d10631de0f33f65adec91e45d9
ChoiGYoung <choidh1261@naver.com>
2023-08-06 18:05:54 +0900

    feat : 근무지 수정 UI 구현
    근무지명, 시급 수정 가능한 근무지 수정 UI 구현
    추후 홈화면에서 근무지리스트뷰 클릭 시 수정 페이지로 이동 하여 수정 가능하도록 구현 예정

---
+++ app/src/main/java/com/cookandroid/capstone/Main_WorkDataActivity.java
@@ -0,0 +1,18 @@
+package com.cookandroid.capstone;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main_WorkDataActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main_workdata);
+
+
+    }
+}
