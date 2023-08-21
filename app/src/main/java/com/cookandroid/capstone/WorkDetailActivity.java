c074a757b5afaed3546a84e6926edca8136e9740
ChoiGYoung <choidh1261@naver.com>
2023-08-22 02:05:47 +0900

    feat : 근무 상세 커스텀리스트뷰 연결 및 날짜 별 급여 출력
    날짜별 급여 내역을 출력하도록 구현

--- app/src/main/java/com/cookandroid/capstone/WorkDetailActivity.java
+++ app/src/main/java/com/cookandroid/capstone/WorkDetailActivity.java
@@ -2,27 +2,49 @@ package com.cookandroid.capstone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 
 public class WorkDetailActivity extends AppCompatActivity {
     private String clickedName; // 클릭한 아이템의 이름을 저장하는 변수
     private String clickedMoney; // 클릭한 아이템의 이름을 저장하는 변수
+    // 사용자 로그인 상태 확인
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    private ArrayList<String> dateList = new ArrayList<>(); // 날짜 목록을 저장할 리스트
+    private ArrayList<String> earningsList = new ArrayList<>(); // 급여 목록을 저장할 리스트
+    private MyAdapter adapter; // 커스텀 어댑터
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workdetail);
 
         TextView name = (TextView) findViewById(R.id.name);
-        TextView money= (TextView) findViewById(R.id.money);
+        TextView money = (TextView) findViewById(R.id.money);
         TextView btnEdit = (TextView) findViewById(R.id.btnEdit);
         TextView btnBack = (TextView) findViewById(R.id.btnBack);
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        // 사용자의 고유한 아이디를 가져옵니다.
+        String userId = currentUser.getUid();
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -37,6 +59,42 @@ public class WorkDetailActivity extends AppCompatActivity {
                 money.setText(clickedMoney);
             }
         }
+
+        adapter = new MyAdapter(dateList, earningsList);
+        listView.setAdapter(adapter);
+
+        // Firebase 데이터베이스에서 데이터 가져오기
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId).child("Data");
+        databaseReference.orderByChild("name").equalTo(clickedName).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                dateList.clear(); // 리스트 초기화
+                earningsList.clear(); // 급여 리스트 초기화
+
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    DataSnapshot datesSnapshot = userSnapshot.child("dates");
+
+                    // dates 하위의 모든 날짜와 급여를 가져와서 리스트에 추가
+                    for (DataSnapshot dateSnapshot : datesSnapshot.getChildren()) {
+                        String dateWithSpaces = dateSnapshot.child("date").getValue(String.class);
+                        Long earningsValue = dateSnapshot.child("earnings").getValue(Long.class);
+                        String earnings = (earningsValue != null) ? String.valueOf(earningsValue) : "";
+                        String dateWithoutSpaces = dateWithSpaces.trim(); // 앞뒤 공백 제거
+                        dateList.add(dateWithoutSpaces);
+                        earningsList.add(earnings);
+                    }
+                }
+
+                // 어댑터에 데이터 변경을 알립니다.
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 데이터 가져오기 실패 시 처리
+            }
+        });
+
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,8 +125,37 @@ public class WorkDetailActivity extends AppCompatActivity {
         });
 
 
+    }
 
+    class MyAdapter extends ArrayAdapter<String> {
+        private ArrayList<String> earningsList; // 이 변수를 추가해줍니다.
 
+        MyAdapter(ArrayList<String> dateList, ArrayList<String> earningsList) {
+            super(WorkDetailActivity.this, R.layout.workdetail_customlistview, dateList);
+            this.earningsList = earningsList;
+        }
 
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = getLayoutInflater().inflate(R.layout.workdetail_customlistview, parent, false);
+            }
+
+            // Firebase에서 가져온 날짜 데이터를 가져와서 설정
+            String date = dateList.get(position);
+            TextView textViewDate = view.findViewById(R.id.textViewDate);
+            textViewDate.setText(date);
+
+            // Firebase에서 가져온 급여 데이터를 가져와서 설정
+            String earnings = this.earningsList.get(position); // 이 부분에서 this를 사용하여 명시적으로 지정
+            TextView textViewEarnings = view.findViewById(R.id.textViewEarnings);
+            textViewEarnings.setText(earnings);
+
+            // 추가로 원하는 뷰에 데이터를 설정할 수 있습니다.
+
+            return view;
+        }
     }
 }
