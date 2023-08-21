f91a1ae1dd9646dec09bf53aacd04e59ca384415
ChoiGYoung <choidh1261@naver.com>
2023-08-21 21:52:46 +0900

    feat : 근무 상세 페이지에 월급 출력
    홈화면에서 근무지 아이템 클릭 시 해당 근무지의 급여를 근무 상세 페이지에 출력

--- app/src/main/java/com/cookandroid/capstone/WorkDetailActivity.java
+++ app/src/main/java/com/cookandroid/capstone/WorkDetailActivity.java
@@ -1,2 +1,74 @@
-package com.cookandroid.capstone;public class WorkDetailActivity {
+package com.cookandroid.capstone;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+
+public class WorkDetailActivity extends AppCompatActivity {
+    private String clickedName; // 클릭한 아이템의 이름을 저장하는 변수
+    private String clickedMoney; // 클릭한 아이템의 이름을 저장하는 변수
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workdetail);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView money= (TextView) findViewById(R.id.money);
+        TextView btnEdit = (TextView) findViewById(R.id.btnEdit);
+        TextView btnBack = (TextView) findViewById(R.id.btnBack);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            clickedName = intent.getStringExtra("clicked_name");
+            clickedMoney = intent.getStringExtra("clicked_money");
+            if (clickedName != null) {
+                // 클릭한 아이템의 이름을 EditText에 출력
+                name.setText(clickedName);
+            }
+            if (clickedMoney != null) {
+                // 클릭한 아이템의 이름을 EditText에 출력
+                money.setText(clickedMoney);
+            }
+        }
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newName = name.getText().toString();
+
+
+                // Intent 생성
+                Intent intent = new Intent(WorkDetailActivity.this, Main_WorkDataActivity.class);
+
+                // 데이터를 Intent에 추가
+                intent.putExtra("clicked_name", newName);
+
+
+                // Intent 시작
+                startActivity(intent);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(WorkDetailActivity.this, MainActivity.class);
+                homeIntent.putExtra("showCommunity", false); // showCommunity 값을 false로 설정
+                homeIntent.putExtra("showCalendar", false); // showCalendar 값을 false로 설정
+                startActivity(homeIntent); // 홈 화면으로 이동
+            }
+        });
+
+
+
+
+
+    }
 }
