25cc34f7f8a1d18e1bb28a1f3055ac87a3ee01bc
ahnjisu <an1302@hs.ac.kr>
2023-10-24 12:50:54 +0900

    feat : 채팅, 채팅 목록 화면 + 데이터
    채팅, 채팅 목록 화면 + 데이터

---
+++ app/src/main/java/com/cookandroid/capstone/Fragment/model/UserDTO.java
@@ -0,0 +1,46 @@
+package com.cookandroid.capstone.Fragment.model;
+
+import androidx.annotation.Nullable;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private String id;
+    private String email;
+    private String nickname;
+    private String avatarUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+}
